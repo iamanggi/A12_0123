5a21bd31aa1ddd43d021729ac198e1ae7edde0f4
iamanggi <anggipuspita452@gmail.com>
2025-01-18 00:51:45 +0700

    Tambah data class model Kursus

---
+++ app/src/main/java/com/example/tugasakhira12/model/Kursus.kt
@@ -0,0 +1,39 @@
+package com.example.tugasakhira12.model
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class KursusDetailResponse(
+    val status: Boolean,
+    val message : String,
+    val data : Kursus
+)
+
+@Serializable
+data class KursusResponse(
+    val status: Boolean,
+    val message : String,
+    val data : List<Kursus>
+)
+
+@Serializable
+data class Kursus (
+    @SerialName("id_kursus")
+    val idKursus: String,
+
+    @SerialName("nama_kursus")
+    val namaKursus: String,
+
+    val deskripsi: String,
+    val kategori: String,
+    val harga: String,
+
+    @SerialName("id_instruktur")
+    val idInstruktur: String
+)
+
+
+
+
+
