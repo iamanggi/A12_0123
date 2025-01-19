7038b701a9bb2d691dc95e068fd2f5ae6b8f8652
iamanggi <anggipuspita452@gmail.com>
2025-01-19 21:32:47 +0700

    Tambah interface SiswaService dengan CRUD API

---
+++ app/src/main/java/com/example/tugasakhira12/Service/Siswa/SiswaService.kt
@@ -0,0 +1,37 @@
+package com.example.tugasakhira12.Service.Siswa
+
+import com.example.tugasakhira12.model.Siswa
+import com.example.tugasakhira12.model.SiswaDetailResponse
+import com.example.tugasakhira12.model.SiswaResponse
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.DELETE
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Path
+
+
+interface SiswaService {
+    @Headers(
+        "Accept: application/json",
+        "Content-Type: application/json"
+    )
+
+    @GET(".")
+    suspend fun getSiswa(): SiswaResponse
+
+    @GET("{id_siswa}")
+    suspend fun getSiswaByID(@Path("id_siswa")id_siswa: String): SiswaDetailResponse
+
+    @POST("add")
+    suspend fun insertSiswa(@Body siswa: Siswa)
+
+    @PUT("{id_siswa}")
+    suspend fun updateSiswa(@Path("id_siswa")id_siswa: String,@Body siswa: Siswa)
+
+    @DELETE("{id_siswa}")
+    suspend fun deleteSiswa(@Path("id_siswa")id_siswa: String): Response<Void>
+
+}
