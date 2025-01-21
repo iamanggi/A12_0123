3ae79f5150ecd05f255f83353c3b3f7967e86e8e
iamanggi <anggipuspita452@gmail.com>
2025-01-21 17:25:01 +0700

    Tambah Komentar pada interface DestinasiNavigation

--- app/src/main/java/com/example/tugasakhira12/ui/navigation/DestinasiNavigation.kt
+++ app/src/main/java/com/example/tugasakhira12/ui/navigation/DestinasiNavigation.kt
@@ -1,5 +1,6 @@
 package com.example.tugasakhira12.ui.navigation
 
+//Tambah interface DestinasiNavigation untuk mendefinisikan rute dan judul di navigasi.
 interface DestinasiNavigation {
     val route: String
     val titleRes: String
