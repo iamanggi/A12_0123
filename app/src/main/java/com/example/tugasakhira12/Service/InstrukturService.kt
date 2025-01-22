1131aa3be57aa7d68bf8a0fb0e459ba345a8d81a
iamanggi <anggipuspita452@gmail.com>
2025-01-23 01:34:00 +0700

    Tambah interface InstrukturService dengan CRUD API

---
+++ app/src/main/java/com/example/tugasakhira12/Service/InstrukturService.kt
@@ -0,0 +1,36 @@
+package com.example.tugasakhira12.Service
+
+import com.example.tugasakhira12.model.InstDetailResponse
+import com.example.tugasakhira12.model.InstResponse
+import com.example.tugasakhira12.model.Instruktur
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.DELETE
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Path
+
+interface InstrukturService {
+    @Headers(
+        "Accept: application/json",
+        "Content-Type: application/json"
+    )
+
+    @GET(".")
+    suspend fun getInstruktur(): InstResponse
+
+    @GET("{id_instruktur}")
+    suspend fun getInstrukturByID(@Path("id_instruktur")id_instruktur: String): InstDetailResponse
+
+    @POST("add")
+    suspend fun insertInstruktur(@Body instruktur: Instruktur)
+
+    @PUT("{id_instruktur}")
+    suspend fun updateInstruktur(@Path("id_instruktur")id_instruktur: String, @Body instruktur: Instruktur)
+
+    @DELETE("{id_instruktur}")
+    suspend fun deleteInstruktur(@Path("id_instruktur")id_instruktur: String): Response<Void>
+
+}
