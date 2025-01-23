6cdb348e5649d0b40635ddab7abcbdc8bedc6822
iamanggi <anggipuspita452@gmail.com>
2025-01-24 03:00:10 +0700

    Tambah implementasi AppContainer dan container untuk PendaftaranRepository

--- app/src/main/java/com/example/tugasakhira12/Repository/MainContainer.kt
+++ app/src/main/java/com/example/tugasakhira12/Repository/MainContainer.kt
@@ -4,10 +4,13 @@ import com.example.tugasakhira12.Repository.Instruktur.InstrukturRepository
 import com.example.tugasakhira12.Repository.Instruktur.NetworkInstrukturRepository
 import com.example.tugasakhira12.Repository.Kursus.KursusRepository
 import com.example.tugasakhira12.Repository.Kursus.NetworkKursusRepository
+import com.example.tugasakhira12.Repository.Pendaftaran.NetworkPendRepository
+import com.example.tugasakhira12.Repository.Pendaftaran.PendaftaranRepository
 import com.example.tugasakhira12.Repository.Siswa.NetworkSiswaRepository
 import com.example.tugasakhira12.Repository.Siswa.SiswaRepository
 import com.example.tugasakhira12.Service.InstrukturService
 import com.example.tugasakhira12.Service.KursusService
+import com.example.tugasakhira12.Service.PendaftaranService
 import com.example.tugasakhira12.Service.SiswaService
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import kotlinx.serialization.json.Json
@@ -18,12 +21,14 @@ interface AppContainer {
     val siswaRepository: SiswaRepository
     val instrukturRepository: InstrukturRepository
     val kursusRepository: KursusRepository
+    val pendaftaranRepository: PendaftaranRepository
 }
 
 class MainAppContainer : AppContainer {
     private val siswaContainer = SiswaContainer()
     private val instrukturContainer = InstrukturContainer()
     private val kursusContainer = KursusContainer()
+    private val pendaftaranContainer = PendaftaranContainer()
 
 
     override val siswaRepository: SiswaRepository
@@ -34,6 +39,9 @@ class MainAppContainer : AppContainer {
 
     override val kursusRepository: KursusRepository
         get() = kursusContainer.kursusRepository
+
+    override val pendaftaranRepository: PendaftaranRepository
+        get() = pendaftaranContainer.pendaftaranRepository
 }
 
 class SiswaContainer  {
@@ -70,4 +78,16 @@ class KursusContainer {
 
     private val kursusService: KursusService by lazy { retrofit.create(KursusService::class.java) }
     val kursusRepository: KursusRepository by lazy { NetworkKursusRepository(kursusService) }
+}
+
+class PendaftaranContainer {
+    private val baseUrl = "http://10.0.2.2:3000/pam/Pendaftaran/"
+    private val json = Json { ignoreUnknownKeys = true }
+    private val retrofit: Retrofit = Retrofit.Builder()
+        .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
+        .baseUrl(baseUrl)
+        .build()
+
+    private val pendaftaranService: PendaftaranService by lazy { retrofit.create(PendaftaranService::class.java) }
+    val pendaftaranRepository: PendaftaranRepository by lazy { NetworkPendRepository(pendaftaranService) }
 }
