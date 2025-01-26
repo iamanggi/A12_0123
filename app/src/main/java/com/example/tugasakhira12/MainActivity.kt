cbe31c23ad28370b579758e1fd2ca5e45776c75e
iamanggi <anggipuspita452@gmail.com>
2025-01-26 23:24:10 +0700

    Update tampilan utama dengan menggunakan KursusApp di MainActivity

--- app/src/main/java/com/example/tugasakhira12/MainActivity.kt
+++ app/src/main/java/com/example/tugasakhira12/MainActivity.kt
@@ -5,12 +5,12 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.tugasakhira12.ui.KursusApp
 import com.example.tugasakhira12.ui.theme.TugasAkhirA12Theme
 
 class MainActivity : ComponentActivity() {
@@ -20,10 +20,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             TugasAkhirA12Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                   KursusApp()
                 }
             }
         }
