73c6beaaa9b5a775f0ea6372700cf3a38107b848
iamanggi <anggipuspita452@gmail.com>
2025-01-26 23:14:34 +0700

    menyederhanakan struktur container untuk semua repository

--- app/src/main/java/com/example/tugasakhira12/Repository/MainContainer.kt
+++ app/src/main/java/com/example/tugasakhira12/Repository/MainContainer.kt
@@ -25,69 +25,33 @@ interface AppContainer {
 }
 
 class MainAppContainer : AppContainer {
-    private val siswaContainer = SiswaContainer()
-    private val instrukturContainer = InstrukturContainer()
-    private val kursusContainer = KursusContainer()
-    private val pendaftaranContainer = PendaftaranContainer()
 
-
-    override val siswaRepository: SiswaRepository
-        get() = siswaContainer.siswaRepository
-
-    override val instrukturRepository: InstrukturRepository
-        get() = instrukturContainer.instrukturRepository
-
-    override val kursusRepository: KursusRepository
-        get() = kursusContainer.kursusRepository
-
-    override val pendaftaranRepository: PendaftaranRepository
-        get() = pendaftaranContainer.pendaftaranRepository
-}
-
-class SiswaContainer  {
-    private val baseUrl = "http://10.0.2.2:3000/pam/Siswa/" // Localhost for emulator
+    private val baseUrl = "http://10.0.2.2:3000/pam/" // Localhost for emulator
     private val json = Json { ignoreUnknownKeys = true }
     private val retrofit: Retrofit = Retrofit.Builder()
         .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
         .baseUrl(baseUrl)
         .build()
 
+    //Siswa
     private val siswaService: SiswaService by lazy { retrofit.create(SiswaService::class.java) }
-     val siswaRepository: SiswaRepository by lazy { NetworkSiswaRepository(siswaService) }
-}
+    override val siswaRepository: SiswaRepository by lazy { NetworkSiswaRepository(siswaService) }
 
-class InstrukturContainer {
-    private val baseUrl = "http://10.0.2.2:3000/pam/Instruktur/"
-    private val json = Json { ignoreUnknownKeys = true }
-    private val retrofit: Retrofit = Retrofit.Builder()
-        .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
-        .baseUrl(baseUrl)
-        .build()
-
+    //instruktur
     private val instrukturService: InstrukturService by lazy { retrofit.create(InstrukturService::class.java) }
-     val instrukturRepository: InstrukturRepository by lazy { NetworkInstrukturRepository(instrukturService) }
-}
+    override val instrukturRepository: InstrukturRepository by lazy { NetworkInstrukturRepository(instrukturService) }
 
-class KursusContainer {
-    private val baseUrl = "http://10.0.2.2:3000/pam/Kursus/"
-    private val json = Json { ignoreUnknownKeys = true }
-    private val retrofit: Retrofit = Retrofit.Builder()
-        .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
-        .baseUrl(baseUrl)
-        .build()
-
+    //kursus
     private val kursusService: KursusService by lazy { retrofit.create(KursusService::class.java) }
-    val kursusRepository: KursusRepository by lazy { NetworkKursusRepository(kursusService) }
-}
+    override val kursusRepository: KursusRepository by lazy { NetworkKursusRepository(kursusService) }
 
-class PendaftaranContainer {
-    private val baseUrl = "http://10.0.2.2:3000/pam/Pendaftaran/"
-    private val json = Json { ignoreUnknownKeys = true }
-    private val retrofit: Retrofit = Retrofit.Builder()
-        .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
-        .baseUrl(baseUrl)
-        .build()
-
+    //pendaftaran
     private val pendaftaranService: PendaftaranService by lazy { retrofit.create(PendaftaranService::class.java) }
-    val pendaftaranRepository: PendaftaranRepository by lazy { NetworkPendRepository(pendaftaranService) }
+    override val pendaftaranRepository: PendaftaranRepository by lazy { NetworkPendRepository(pendaftaranService) }
+
 }
+
+
+
+
+
